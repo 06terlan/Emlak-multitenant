2a98744eab368533a0291182d3e4abe36d2329a5
Huseynzade <huseynzada.m@gmail.com>
2018-02-05 10:00:12 -0800

    text edit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,17 +15,17 @@
                     
                     <div class="loginContainer">
                         <div class="loginForms">
-                            <label>Login</label>
+                            <label>İstİfadəçİ adı</label>
                             <input id="login" type="text" class="form-control" name="login" value="{{ old('login') }}" required autofocus placeholder="" />
                         </div><!--loginForms-->
                     <div class="loginForms">
-                            <label>Password</label>
+                            <label> İstİfadƏçİ Parolu</label>
                             <input id="password" type="password" class="form-control inputText" name="password" required placeholder="" />
                     </div>
                         </div>
                     <div class="loginForms">     
                         {{ csrf_field() }}                   
-                        <input type="submit" class="confirmBtn" style="cursor:pointer;" value="Login" />
+                        <input type="submit" class="confirmBtn" style="cursor:pointer;" value="Daxİl Ol" />
                     </div>
                     <div class="loginlostPass">
                         <a href="#">Parolu bərpa et</a>
